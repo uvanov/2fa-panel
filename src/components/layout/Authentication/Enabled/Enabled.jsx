7873d4e7e6250d96a9554b99f20ab4fb2cea6082
uvanov <kirill.ivanov000@gmail.com>
2022-07-10 19:06:31 +0300

    Authentication/Enabled Layout

---
+++ src/components/layout/Authentication/Enabled/Enabled.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from 'react';
+
+import { StepContext } from '../../TwoFactor/TwoFactor';
+
+import { Flex, Icon, Typography } from '../../../ui/ui';
+import Methods from '../../../ui/Methods/Methods';
+
+const Enabled = () => {
+
+  const [selectedMethod, setSelectedMethod] = useState('application'); // application, telegram
+  const { setStep } = useContext(StepContext);
+
+  return (
+    <div>
+      <Methods>
+        <Methods.Method
+          icon=''
+          iconColor='#BC57EB'
+          title='Приложение для создания 2FA-кодов'
+          selected={ selectedMethod === 'application' }
+          onSelect={ () => setSelectedMethod('application') }
+        >
+          <a
+            href='#'
+            onClick={ () => setStep('reserve') }
+          >
+            резервные коды доступа
+          </a>
+        </Methods.Method>
+        <Methods.Method
+          icon=''
+          iconColor='#2D9CDB'
+          title='Коды от Телеграм-бота @omnidesk_2fa_bot'
+          description='Вы привязали Телеграм-аккаунт @serj_preobraj, привязать другой аккаунт'
+          selected={ selectedMethod === 'telegram' }
+          onSelect={ () => setSelectedMethod('telegram') }
+        >
+          Вы привязали Телеграм-аккаунт @serj_preobraj,
+          <a
+            href='#'
+            onClick={ () => setStep('additional') }
+          >
+             привязать другой аккаунт
+          </a>
+        </Methods.Method>
+      </Methods>
+    </div>
+  );
+};
+
+export default Enabled;
