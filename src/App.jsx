81a8aae14e2df6f3ea736bd5fe2051130ad7b4aa
uvanov <kirill.ivanov000@gmail.com>
2022-07-11 13:51:17 +0300

    Root component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import { Grid } from './components/ui/ui';
+
+import Header from './components/layout/Header/Header';
+import Sidebar from './components/layout/Sidebar/Sidebar';
+import Container from './components/layout/Container/Container';
+import Content from './components/layout/Content/Content';
+
+import StyledApplicationGrid from './App.styles';
+
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header/>
+      <Container>
+        <StyledApplicationGrid
+          columns={ ['1fr', '3fr'] }
+          columnGap={ 20 }
+        >
+          <Sidebar/>
+          <Content />
+        </StyledApplicationGrid>
+      </Container>
+    </>
   );
 };
 
